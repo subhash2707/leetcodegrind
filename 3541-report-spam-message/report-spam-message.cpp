ea45cfe7c53603c8efbafc45b782cee1025b90c2
subhash2707 <150540195+subhash2707@users.noreply.github.com>
2025-02-10 12:34:46 +0530

    Time: 497 ms (23.98%) | Memory: 288.8 MB (50.41%) - LeetSync

--- 3541-report-spam-message/report-spam-message.cpp
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -1,3 +1,24 @@
+//sol1
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+    unordered_set<string>st(bannedWords.begin(),bannedWords.end());
+     int cnt =0;//count matches
+     //iterate over message and check if bannedword match with bannedword
+     for(auto it:message) {
+        if(st.find(it)!=st.end()) 
+        cnt++;
+     }  
+     return cnt>1;     
+    }
+};
+
+
+
+
+
+/*---.MY SOL(INP TO REMOVE ELEMENT FROM MAP WHEN COUNTED)
+
 class Solution {
 public:
     bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
@@ -19,3 +40,4 @@ public:
     }
 
 };
+*/
