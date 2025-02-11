9703bbcbece308daf6391ba83dacc8a64a82e8f0
subhash2707 <150540195+subhash2707@users.noreply.github.com>
2025-02-11 23:30:09 +0530

    Time: 10 ms (46.04%), Space: 11.3 MB (83.28%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,6 +8,8 @@
  */
 
  /////////  m1 USING VISITED MAP RETURN ALREADY MARKED NODE
+ //TC-(N),SC-(ON)
+ /*
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
@@ -22,5 +24,36 @@ public:
           temp=temp->next;  
      }  
      return nullptr; 
+    }
+};
+*/
+
+
+
+
+
+// M2 ---- FAST AND SLOW POINTERS
+// start slow from head and when it meets fast again then it is stating point of cycle
+class Solution {
+ListNode* starting(ListNode* fast,ListNode* head){
+    ListNode* temp=head;
+    while(temp!=fast){
+        temp=temp->next;
+        fast=fast->next;
+    }
+    return fast;//temp
+
+}
+public:
+    ListNode *detectCycle(ListNode *head) {
+     ListNode* fast=head;
+     ListNode* slow=head;
+    while(fast && fast->next){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast)
+        return starting(fast,head);
+    }
+   return nullptr;
     }
 };
