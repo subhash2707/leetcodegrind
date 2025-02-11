ae0c6d32712b7ffd3ab07ee040793b65373d331e
subhash2707 <150540195+subhash2707@users.noreply.github.com>
2025-02-11 23:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.06%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,13 +1,35 @@
+// SOL2 USING RECURSION
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if (b == 0) return a;  // Base case: No carry left
+        int sum = a ^ b;       // Sum without carry
+        int carry = (a & b) << 1; // Carry shifted left
+        return getSum(sum, carry); // Recursive call
+    }
+};
+
+
+
+
+
 //sol
+/*
 class Solution {
 public:
     int getSum(int a, int b) {
         while (b != 0) {
-            int sum = a ^ b; // sum without carry
-            int carry = (a & b) << 1; // carry shifted left
+            int sum = a ^ b; 
+            int carry = (a & b) << 1;
             a = sum;
             b = carry;
         }
         return a;
     }
 };
+*/
+
+
+
+
+
