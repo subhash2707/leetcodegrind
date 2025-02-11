dd7a3bb3bd6729d70bafc5b94bb97c1a0d436563
subhash2707 <150540195+subhash2707@users.noreply.github.com>
2025-02-11 16:21:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,3 +1,6 @@
+//1.use stack
+//2.use fast and slow pointer and reverse after slow
+//3. find len and revese after len/2
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,7 +11,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+// m1 usign stack
+class Solution {
+    public:
+
+ bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+        stack<int>st;
+ListNode* temp=head;
+while(temp){
+    st.push(temp->val);
+    temp=temp->next;
+}
+
+temp=head;
+while(temp){
+    if(temp->val!=st.top()) return false;
+    st.pop();
+    temp=temp->next;
+}
+return true;
+        
+ }
+ };
+
+
+
+
 /*
+// METHOD 3-------------------------------------------
 class Solution {
     int len(ListNode* head) {
         ListNode* temp = head;
@@ -102,7 +135,7 @@ public:
     bool isPalindrome(ListNode* head) {
         if(!head || !head->next) return true;
     int length=len(head);
-    int mid=length/2;//first mid for even
+    int mid=(length+1)/2;//first mid for even
     ListNode* temp=head;
 //     int cnt=0;
 //     while(cnt<mid){
@@ -126,7 +159,7 @@ public:
     // if(len%2==0) rev(mid->next);
     // else rev(mid->next)
     //reverse from mid-next
-    if (length % 2 != 0) temp = temp->next;
+  //  if (length % 2 != 0) temp = temp->next;
     
     ListNode* newhead=reverse(temp->next);
     
